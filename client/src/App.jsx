885531cf430ef8a6cb44d6c2ce1276822018ea7a
Alvin Rachman Septian <alvinrachman16@gmail.com>
2025-01-10 15:59:08 +0700

    create client side

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import InputTask from "./components/InputTask";
+import ListTask from "./components/ListTask";
+
+function App() {
+  return (
+    <main className="flex justify-center items-center container px-4 max-w-full h-screen bg-gradient-to-br from-gray-800 via-gray-900 to-indigo-950">
+      <section className="w-full p-5 bg-sky-950 rounded-md text-slate-100">
+        <h1 className="text-center text-2xl font-bold">Task App</h1>
+        <InputTask />
+        <ListTask />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/components/InputTask.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function InputTask() {
+  return (
+    <div className="my-5 text-sm">
+      <input type="text" className="w-3/4 rounded-s-md py-1 pl-3" />
+      <button
+        type="button"
+        className="bg-sky-900 hover:bg-sky-800 rounded-r-md w-1/4 py-1 font-semibold"
+      >
+        + Add
+      </button>
+    </div>
+  );
+}
+
+export default InputTask;
